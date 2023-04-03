33c969b64a1da6647e95b1d37cc76851ba01e362
Jesse Hidalgo <jesse_hidalgo8@yahoo.com>
2023-04-03 18:50:25 -0400

    Added First and Last name fields for a new user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :email, :user_name, presence: :true, uniqueness: :true
+  validates :first_name, :last_name, presence: :true
   has_many :discussions
   has_many :book_statuses
   has_many :books, through: :book_statuses
